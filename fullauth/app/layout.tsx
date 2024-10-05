3f3a10ff7443b2694efcf7f6b7b405b33bd27d2f
Maciej <mackolinop@gmail.com>
2024-10-06 01:45:43 +0200

    Add frontend

---
+++ fullauth/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "@/styles/globals.css";
+import Provider from "@/redux/provider";
+//import Navbar from "@/components/common/Navbar";
+//import Footer from "@/components/common/Footer";
+import { Navbar, Footer } from "@/components/common"; // possible with the index.ts
+
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Full Auth",
+  description: "Full Auth application that provides jwt authentication",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Provider>
+          <Navbar></Navbar>
+          <div>{children}</div>
+          <Footer></Footer>
+        </Provider>
+      </body>
+    </html>
+  );
+}
+
+
+// Finished on 34:50
